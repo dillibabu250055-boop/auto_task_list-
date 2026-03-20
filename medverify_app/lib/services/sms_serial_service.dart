6a8f996e4e3c4f1dd8212e69ee4777316517a9f4
dillibabu250055-boop <dillibabu.250055@ece.ritchennai.edu.in>
2026-03-20 20:25:54 +0530

    Delete medverify_app directory

--- medverify_app/lib/services/sms_serial_service.dart
+++
@@ -1,2 +0,0 @@
-export 'sms_serial_service_stub.dart'
-    if (dart.library.io) 'sms_serial_service_native.dart';
--- medverify_app/lib/services/sms_serial_service_stub.dart
+++
@@ -1,6 +0,0 @@
-class SMSSerialService {
-  bool initialize() => false;
-  void sendSMS(String phone, String message, {bool isTamil = false}) {}
-  static String toUCS2Hex(String text) => "";
-  void dispose() {}
-}
